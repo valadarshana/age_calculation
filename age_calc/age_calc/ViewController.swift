ba03a28985b9810adea97babe7f1f89644e0986e
Vijay Parmar <vspxcoder@gmail.com>
2020-09-23 09:49:09 +0530

    UI Added

---
+++ age_calc/age_calc/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  age_calc
+//
+//  Created by Vijay Parmar on 22/09/20.
+//  Copyright © 2020 WD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var txt_date: UITextField!
+    
+    @IBOutlet weak var lbl_cal: UILabel!
+    
+    @IBOutlet weak var dateview: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        //dateview.layer.cornerRadius=(dateview.frame.size.height/2)
+    }
+
+
+}
+
